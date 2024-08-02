96205fac0ee9b09248789877780731a78ac4037a
Jack <99113423+mirrorjack@users.noreply.github.com>
2024-08-02 23:10:52 +0300

    Added "Size Increasing"

--- Bongo Pig/Assets/PigScript.cs
+++ Bongo Pig/Assets/PigScript.cs
@@ -9,17 +9,34 @@ public class PigScript : MonoBehaviour
     public float flapForce;
     public LogicScript logicScript;
     public bool birdIsAlive = true;
-    // Start is called before the first frame update
+    bool firstSizeIncreasing = false;
+    bool secondSizeIncreasing = false;
+    bool thirdSizeIncreasing = false;
     [Header("Animation settings")]
     public Animator animator;
     void Start()
-    {
+    { 
         Time.timeScale = 0f;
         logicScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
     // Update is called once per frame
     void Update()
     {
+        if ((logicScript.playerScore == 5)&&(firstSizeIncreasing == false))
+        {
+            transform.localScale = transform.localScale + Vector3.one;
+            firstSizeIncreasing = true;
+        }
+        if ((logicScript.playerScore == 10) && (secondSizeIncreasing == false))
+        {
+            transform.localScale = transform.localScale + Vector3.one;
+            secondSizeIncreasing = true;
+        }
+        if ((logicScript.playerScore == 15) && (thirdSizeIncreasing == false))
+        {
+            transform.localScale = transform.localScale + Vector3.one;
+            thirdSizeIncreasing = true;
+        }
         if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             slotforrigidbody.velocity = Vector2.up * flapForce;
